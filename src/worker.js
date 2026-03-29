e32f06352b69ed22b77b89c121ce3613b4c300a8
edwin vakkachan <edwinvakkachan@gmail.com>
2026-03-29 11:34:50 +0530

    deualy 1 sec

--- src/worker.js
+++ src/worker.js
@@ -29,7 +29,7 @@ async function processBatch() {
 
         // Dispatch based on target
         if (msg.target === "telegram") {
-          await delay(200,true);
+          await delay(1000,true);
           console.log(msg.payload.message)
           await sendTelegramMessage(msg.payload.message);
         }
